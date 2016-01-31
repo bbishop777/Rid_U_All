74267df18a9297dfabcb6a14703ae073e7aae5b2
BBishop <brad.bishop68@gmail.com>
2016-01-31 12:08:58 -1000

    Adds explosion radius that eliminates enemies

--- Assets/Scripts/RadiusController.cs
+++ Assets/Scripts/RadiusController.cs
@@ -7,8 +7,11 @@ public class RadiusController : MonoBehaviour {
 	public Text winText;
 	public GameObject player;
 	private int count;
+	private bool triggerPressed;
+	private float rad = 7.5f;
 
 	private Vector3 offset;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -16,37 +19,56 @@ public class RadiusController : MonoBehaviour {
 		SetCountText ();
 		winText.text = "";
 		offset = transform.position - player.transform.position;
-		count = 0;
-		SetCountText ();
-		winText.text = "";
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () 
-	{
-		//transform.position = player.transform.position;
+	{	
 		transform.position = player.transform.position + offset;
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			ExplosionDamage (transform.position, rad);
+//			triggerPressed = true;
+		}
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			triggerPressed = false;
+		}
 	}
+
+	void ExplosionDamage(Vector3 center, float radius) {
+		Collider[] hitColliders = Physics.OverlapSphere(center, radius);
+		int i = 0;
+		while (i < hitColliders.Length) {
+			hitColliders[i].gameObject.SetActive (false);
+			i++;
+		}
+	}
+
 
 	void OnTriggerEnter(Collider other)
 	{
-//		if(other.gameObject.CompareTag("Pick Up"))
-//		{
-//			other.gameObject.SetActive (false);
-//			count = count + 1;
-//			SetCountText ();
-//		}
+		if(other.gameObject.CompareTag("Pick Up") && triggerPressed == true)
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
 		if (other.gameObject.CompareTag ("Player")) 
 		{
 			Physics.IgnoreCollision (other.gameObject.transform.GetComponent<Collider> (), GetComponent<Collider> ());
 		}
-		if(other.gameObject.CompareTag("Pick Up"))
-		{
-			//other.gameObject.SetActive (false);
-			count = count + 1;
-			SetCountText ();
-		}
+//		if(other.gameObject.CompareTag("Pick Up"))
+//		{
+//			//other.gameObject.SetActive (false);
+//			count = count + 1;
+//			SetCountText ();
+//			if (count >= 14) {
+//				winText.text = "You Win!";
+//			}
+//		}
 	}
+
 	void SetCountText ()
 	{
 		countText.text = "Count: " + count.ToString ();
